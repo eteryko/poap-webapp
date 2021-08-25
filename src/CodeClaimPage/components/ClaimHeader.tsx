0e8b765e0a5c4b8c559a07f31ea3daa7686a7576
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2021-08-25 11:26:47 -0300

    ✨ Add: check on success

--- src/CodeClaimPage/components/ClaimHeader.tsx
+++ src/CodeClaimPage/components/ClaimHeader.tsx
@@ -2,24 +2,27 @@ import React from 'react';
 
 /* Assets */
 import ClaimCover from '../../images/claim_cover_blue.svg';
-// import ClaimCoverSuccess from "../../images/claim_cover_blue.svg";
+import ClaimCoverSuccess from '../../images/claim_cover_green.svg';
+import CheckGreen from '../../images/check_green.svg';
 
 /*
  * @dev: Common header for QR claim system
  * */
-const ClaimHeader: React.FC<{ title: string; image?: string; claimed?: boolean }> = ({
+const ClaimHeader: React.FC<{ title: string; image?: string; claimed?: boolean; success?: boolean }> = ({
   title,
   image,
   claimed = true,
+  success,
 }) => {
   return (
     <div className={'claim-header'}>
-      <img src={ClaimCover} alt="cover" className="claim-cover" />
+      <img src={success ? ClaimCoverSuccess : ClaimCover} alt="cover" className="claim-cover" />
       <div className="claim-title-logo-container">
         <div className="logo-event">
-          <img src={image} alt="event" />
+          <img src={image} alt="event" className="logo-img" />
+          {success && <img src={CheckGreen} alt="check!" className="check" />}
         </div>
-        <div className={'title'}>{title}</div>
+        <h1 className="claim-title">{title}</h1>
       </div>
     </div>
   );
