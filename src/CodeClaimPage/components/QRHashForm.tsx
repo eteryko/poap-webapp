a59ea74ce3b5b6b7f691259a4f3ab96d05653ee4
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2021-08-25 10:50:13 -0300

    ✨ Add: claim header and footer

--- src/CodeClaimPage/components/QRHashForm.tsx
+++ src/CodeClaimPage/components/QRHashForm.tsx
@@ -30,38 +30,36 @@ const QRHashForm: React.FC<{
 
   return (
     <div className={'container'}>
-      <div>
-        <Formik
-          enableReinitialize
-          onSubmit={handleForm}
-          initialValues={{ hash: qrHash }}
-          validationSchema={ClaimHashSchema}
-        >
-          {({ dirty, isValid }) => {
-            return (
-              <Form className="claim-form">
-                <div className={'web3-browser'}>Please complete the form below to continue</div>
-                <Field
-                  name="hash"
-                  render={({ field, form }: FieldProps) => {
-                    return (
-                      <input
-                        type="text"
-                        autoComplete="off"
-                        className={classNames(!!form.errors[field.name] && 'error')}
-                        placeholder={'Six-digit code'}
-                        {...field}
-                      />
-                    );
-                  }}
-                />
-                {error && <p className={'bk-msg-error'}>We couldn't find the code, please try again.</p>}
-                <SubmitButton text="Continue" isSubmitting={loading} canSubmit={isValid && dirty} />
-              </Form>
-            );
-          }}
-        </Formik>
-      </div>
+      <Formik
+        enableReinitialize
+        onSubmit={handleForm}
+        initialValues={{ hash: qrHash }}
+        validationSchema={ClaimHashSchema}
+      >
+        {({ dirty, isValid }) => {
+          return (
+            <Form className="claim-form">
+              <div className={'web3-browser'}>Please complete the form below to continue</div>
+              <Field
+                name="hash"
+                render={({ field, form }: FieldProps) => {
+                  return (
+                    <input
+                      type="text"
+                      autoComplete="off"
+                      className={classNames(!!form.errors[field.name] && 'error')}
+                      placeholder={'Six-digit code'}
+                      {...field}
+                    />
+                  );
+                }}
+              />
+              {error && <p className={'bk-msg-error'}>We couldn't find the code, please try again.</p>}
+              <SubmitButton text="Continue" isSubmitting={loading} canSubmit={isValid && dirty} />
+            </Form>
+          );
+        }}
+      </Formik>
     </div>
   );
 };
