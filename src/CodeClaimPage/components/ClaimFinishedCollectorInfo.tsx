dd23227c1f2bcf294dc767cf5e414dc256ae0bd4
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2021-08-26 23:20:47 -0300

    💄 Style: claim finished stats icons

--- src/CodeClaimPage/components/ClaimFinishedCollectorInfo.tsx
+++ src/CodeClaimPage/components/ClaimFinishedCollectorInfo.tsx
@@ -5,10 +5,14 @@ import { HashClaim } from '../../api';
 import StatsImageRight from '../../images/claim_stats_misc_right.svg';
 import StatsImageLeft from '../../images/claim_stats_misc_left.svg';
 import ClaimFinishedStatCard from './ClaimFinishedStatCard';
+import ClaimCalendarIcon from '../../images/claim_finished_calendar.svg';
+import ClaimWalletIcon from '../../images/claim_finished_wallet.svg';
 
 const ClaimFinishedCollectorInfo: React.FC<{ claim: HashClaim }> = ({ claim }) => {
   const claimedWithEmail = !!(claim && claim.claimed && claim.user_input && isValidEmail(claim.user_input));
   const appLink = claimedWithEmail ? `/scan/${claim.user_input}` : `/scan/${claim.beneficiary}`;
+  let yearString = new Date().getFullYear().toString();
+  yearString = yearString.substring(0, 2) + ' ' + yearString.substring(2, 4);
 
   return (
     <div className="claim-collector-info-container">
@@ -16,9 +20,9 @@ const ClaimFinishedCollectorInfo: React.FC<{ claim: HashClaim }> = ({ claim }) =
       <div className={'claim-collector-info-title'}>Keep growing your collection</div>
       <div className={'claim-collector-info-stats'}>
         <img src={StatsImageLeft} alt="" className="claim-collector-info-img-left" />
-        <ClaimFinishedStatCard text="Total POAPs in your wallet" stat="64" />
-        <ClaimFinishedStatCard text="New POAPs this year" stat="27" />
-        <ClaimFinishedStatCard text="New POAPs this month" stat="3" />
+        <ClaimFinishedStatCard text="Total POAPs in your wallet" stat="64" icon={ClaimWalletIcon} />
+        <ClaimFinishedStatCard text="New POAPs this year" stat="27" iconText={yearString} />
+        <ClaimFinishedStatCard text="New POAPs this month" stat="3" icon={ClaimCalendarIcon} />
         <img src={StatsImageRight} alt="" className="claim-collector-info-img-right" />
       </div>
       <LinkButton text={'Browse my collection'} link={appLink} />
--- src/CodeClaimPage/components/ClaimFinishedStatCard.tsx
+++ src/CodeClaimPage/components/ClaimFinishedStatCard.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const ClaimFinishedStatCard: React.FC<{ text: string; stat: string }> = ({ text, stat }) => {
+const ClaimFinishedStatCard: React.FC<{ text: string; stat: string; icon?: string; iconText?: string }> = ({
+  text,
+  stat,
+  icon,
+  iconText,
+}) => {
   return (
     <div className="claim-finished-stat-card">
       <div className="claim-finished-stat-card-text-icon">
+        {icon && <img src={icon} alt="icon" className="claim-finished-stat-card-icon" />}
+        {!icon && iconText && <div className="claim-finished-stat-card-icon-text">{iconText}</div>}
         <div className="claim-finished-stat-card-text">{text}</div>
       </div>
       <div className="claim-finished-stat-card-stat">{stat}</div>
